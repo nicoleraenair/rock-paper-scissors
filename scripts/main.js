ee428a944f4968c30efa8768c0ebca7fc08e4ad6
Nicole Nair <nicoleraenair@gmail.com>
2021-08-18 21:48:21 -0400

    Add basic UI without styling

--- scripts/main.js
+++ scripts/main.js
@@ -1,75 +1,180 @@
+let playerScore = 0;
+let computerScore = 0;
+
+//input div - including choose instructions and rps buttons
+const input = document.createElement("div");
+input.setAttribute('id', 'input');                              
+const choose = document.createElement("p");
+const rockBtn = document.createElement("button");
+const paperBtn = document.createElement("button");
+const scissorsBtn = document.createElement("button");
+
+choose.textContent = "Choose one:";
+input.appendChild(choose);
+
+rockBtn.textContent = "Rock";
+paperBtn.textContent = "Paper";
+scissorsBtn.textContent = "Scissors";
+input.appendChild(rockBtn);
+input.appendChild(paperBtn);
+input.appendChild(scissorsBtn);
+
+rockBtn.addEventListener('click', () => {
+    playRound(rockBtn.textContent);
+});
+paperBtn.addEventListener('click', () => {
+    playRound(paperBtn.textContent);
+});
+scissorsBtn.addEventListener('click', () => {
+    playRound(scissorsBtn.textContent);
+});
+
+
+//scoreboard div - title and player/computer scores
+const scoreboard = document.createElement('div');
+const pscore = document.createElement("p");
+const cscore = document.createElement("p");
+const scoreTitle = document.createElement("p");
+scoreTitle.textContent = "Score:";
+scoreboard.appendChild(scoreTitle);
+scoreboard.appendChild(pscore);
+scoreboard.appendChild(cscore);
+
+//final result card
+let resultCard = document.createElement("div");
+let finalScore = document.createElement("p");
+let result = document.createElement("p");
+resultCard.appendChild(result);
+resultCard.appendChild(finalScore);
+
+//play again button
+let playAgainBtn = document.createElement("button");
+playAgainBtn.textContent = "Play Again";
+playAgainBtn.addEventListener('click', startGame);
+playAgainBtn.addEventListener('click', () => {
+    playAgainBtn.remove();;
+});   
+playAgainBtn.addEventListener('click', () => {
+    resultCard.remove();;
+});  
+
+//body and start button
+const body = document.querySelector("body");
+const startBtn = document.querySelector('#start');
+
+startBtn.addEventListener('click', startGame);
+startBtn.addEventListener('click', () => {
+    startBtn.remove();;
+  });
+
+
+function startGame() {
+    playerScore = 0;
+    computerScore = 0;
+
+    pscore.textContent = "You: 0";
+    cscore.textContent = "Computer: 0"
+    
+    body.appendChild(scoreboard);
+
+    body.appendChild(input);
+}
+
+
 let computerPlay = () => {
     let hand = Math.floor(Math.random() * 3)
     switch (hand) {
         case 0:
-            return "rock"
+            return "Rock"
         case 1:
-            return "paper"
+            return "Paper"
         case 2:
-            return "scissors"
+            return "Scissors"
     }
 };
 
 let playRound = (player) => {
-    player = player.toLowerCase()
     computer = computerPlay()
-    console.log("You played: " + player)
-    console.log("Computer played: " + computer)
+
+    let result = document.createElement("p");
+    let playerSelection = document.createElement("p");
+    let computerSelection = document.createElement("p");
+    playerSelection.textContent = "You played: " + player;
+    computerSelection.textContent = "Computer played: " + computer;
 
     if (player === computer) {
-        return "TIE"
-    }
-
-    switch (player) {
-        case "rock":
-            if (computer === "paper"){
-                return "YOU LOSE"
-            }
-            else {
-                return "YOU WIN"
-            }
-        case "paper":
-            if (computer === "scissors"){
-                return "YOU LOSE"
-            }
-            else {
-                return "YOU WIN"
-            }
-        case "scissors":
-            if (computer === "rock"){
-                return "YOU LOSE"
-            }
-            else {
-                return "YOU WIN"
-            }
-    }
-};
-
-let game = () => {
-    let playerScore = 0
-    let computerScore = 0
-
-    for (let i = 0; i < 5; i++){
-        let player = window.prompt("Rock, Paper, or Scissors? ")
-        let result = playRound(player)
-        console.log(result)
-        if (result === "YOU WIN") {
-            playerScore++
-        }
-        else if (result === "YOU LOSE") {
-            computerScore++
-        }
-    }
-
-    console.log("FINAL RESULT: ")
-    if (playerScore === computerScore) {
-        console.log("TIE!")
-    }
-    else if (playerScore > computerScore){
-        console.log("YOU WON!")
+        result.textContent = ("TIE");
     }
     else {
-        console.log("YOU LOST!")
+        switch (player) {
+            case "Rock":
+                if (computer === "Paper"){
+                    result.textContent = ("YOU LOSE");
+                    computerScore++;
+                }
+                else {
+                    result.textContent = ("YOU WIN");
+                    playerScore++;
+                }
+                break;
+            case "Paper":
+                if (computer === "Scissors"){
+                    result.textContent = ("YOU LOSE");
+                    computerScore++;
+                }
+                else {
+                    result.textContent = ("YOU WIN");
+                    playerScore++;
+                }
+                break;
+            case "Scissors":
+                if (computer === "Rock"){
+                    result.textContent = ("YOU LOSE");
+                    computerScore++;
+                }
+                else {
+                    result.textContent = ("YOU WIN");
+                    playerScore++;
+                }
+                break;
+        }
+
+        pscore.textContent = "You: " + playerScore;
+        cscore.textContent = "Computer: " + computerScore;
     }
-    console.log("Final Score: " + playerScore + " - " + computerScore)
+
+    let resultCard = document.createElement("div");
+    resultCard.appendChild(playerSelection);
+    resultCard.appendChild(computerSelection);
+    resultCard.appendChild(result);
+    body.appendChild(resultCard);
+
+    sleep(1000).then(() => {
+        resultCard.remove();
+        if (playerScore === 5 || computerScore === 5) {
+            endGame();
+        }
+    });
 };
+
+function sleep (time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+function endGame() {
+    input.remove();
+    scoreboard.remove();
+    
+    if (playerScore > computerScore) {
+        result.textContent = "YOU WON!"
+    }
+    else {
+        result.textContent = "YOU LOST!";
+    }
+
+    finalScore.textContent = "Final Score: " + playerScore + " - " + computerScore;
+    
+    body.appendChild(resultCard);
+    body.appendChild(playAgainBtn);
+
+}
